80d9240d8ec01fe52c7b3a740a1e0cf21f031ec4
ibaev <ibaev@vbankcenter.ru>
2025-08-25 10:37:58 +0300

    commit

--- app/src/main/java/ru/ibaevzz/vibehack/domain/model/Volunteer.kt
+++ app/src/main/java/ru/ibaevzz/vibehack/domain/model/Volunteer.kt
@@ -3,7 +3,6 @@ package ru.ibaevzz.vibehack.domain.model
 data class Volunteer(
     val id: Int,
     val name: String,
-    val phoneNumber: String,
     val status: VolunteerRequestStatus,
     val location: String,
     val skills: List<String>,
